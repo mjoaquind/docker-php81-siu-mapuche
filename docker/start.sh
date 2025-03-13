659017bf74cba216cec2420535635e14d65a87c4
Joaquín Díaz <joaquin.diaz@uner.edu.ar>
2025-03-13 09:48:12 -0300

    edito start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
+
 rm /etc/apache2/sites-enabled/000-default.conf
 
 ln -s /data/scripts/mapuche.conf /etc/apache2/sites-enabled/mapuche.conf
